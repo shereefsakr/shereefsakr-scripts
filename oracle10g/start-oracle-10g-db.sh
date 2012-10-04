31caea12f5b81f82f3fa326e1557905604f0c0b3
Shereef Sakr <shereef.sakr@gmail.com>
2012-10-04 13:04:09 +0200

    oracle 10g and tomcat scripts

---
+++ oracle10g/start-oracle-10g-db.sh
@@ -0,0 +1,3 @@
+export ORACLE_HOME=/home/oracle/oracle/product/10.2.0/db_1/
+$ORACLE_HOME/bin/dbstart
+
---
+++ tomcat/start-tomcat.sh
@@ -0,0 +1,4 @@
+TOMCAT_HOME=/home/oracle/CI/install/apache-tomcat-6.0.29
+export JAVA_HOME=/home/oracle/CI/install/jdk1.6.0_35
+$TOMCAT_HOME/bin/startup.sh
+
