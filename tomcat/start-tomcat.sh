2968466f788a6c9d20adf11d8a9d5e3ed1424635
Shereef Sakr <shereef.sakr@gmail.com>
2012-10-04 13:27:50 +0200

    add debug to tomcat scripts

--- tomcat/start-tomcat.sh
+++ tomcat/start-tomcat.sh
@@ -1,4 +1,7 @@
-TOMCAT_HOME=/home/oracle/CI/install/apache-tomcat-6.0.29
 export JAVA_HOME=/home/oracle/CI/install/jdk1.6.0_35
+export JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=9999,server=y,suspend=n"
+
+TOMCAT_HOME=/home/oracle/CI/install/apache-tomcat-6.0.29
+
 $TOMCAT_HOME/bin/startup.sh
 
