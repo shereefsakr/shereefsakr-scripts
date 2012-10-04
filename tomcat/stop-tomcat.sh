dbe653782f2d194ce8c4b421bd557c9a003fdb4f
Shereef Sakr <shereef.sakr@gmail.com>
2012-10-04 13:32:17 +0200

    editing debug for stop-tomcat.sh

--- tomcat/stop-tomcat.sh
+++ tomcat/stop-tomcat.sh
@@ -1,5 +1,5 @@
 export JAVA_HOME=/home/oracle/CI/install/jdk1.6.0_35
-export JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=9999,server=y,suspend=n"
+#export JAVA_OPTS=""
 
 TOMCAT_HOME=/home/oracle/CI/install/apache-tomcat-6.0.29
 
