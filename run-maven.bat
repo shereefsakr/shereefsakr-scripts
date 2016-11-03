6fc7980cff2fa9777e3191e0c27801ec1890b963
shereefsakr <shereef.sakr@gmail.com>
2016-11-03 18:38:32 +0200

    Modification to run-maven.bat

--- run-maven.bat
+++ run-maven.bat
@@ -3,7 +3,9 @@ SET JAVA_HOME=C:\Program Files\Java\jdk1.8.0_91
 SET PROJ_PATH=D:\shereefsakr-dev-work\shereefsakr-experiments\TestThrift
 SET CURR_PATH=%CD%
 
-cd %D:\shereefsakr-dev-work\shereefsakr-experiments\TestThrift%
+echo %CURR_PATH%
+cd %PROJ_PATH%
+echo %CURR_PATH%
 
 %MVN_PATH%\bin\mvn install
 
