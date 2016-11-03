2a3f3cbcc7a6034e6ba793b0b4a6233376ed5f7f
shereefsakr <shereef.sakr@gmail.com>
2016-11-03 18:41:04 +0200

    Final run-maven script

--- run-maven.bat
+++ run-maven.bat
@@ -3,11 +3,11 @@ SET JAVA_HOME=C:\Program Files\Java\jdk1.8.0_91
 SET PROJ_PATH=D:\shereefsakr-dev-work\shereefsakr-experiments\TestThrift
 SET CURR_PATH=%CD%
 
-echo %CURR_PATH%
+echo "Changing current path to %PROJ_PATH%"
 cd %PROJ_PATH%
-echo %CURR_PATH%
 
-%MVN_PATH%\bin\mvn install
 
-echo %CURR_PATH%
+call %MVN_PATH%\bin\mvn install
+
+echo "Changing current path back to current path : %CURR_PATH%"
 cd %CURR_PATH%
