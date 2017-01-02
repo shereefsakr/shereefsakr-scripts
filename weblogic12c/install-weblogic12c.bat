5147f8ab6c2dfd707a61b38bccd125966f770118
shereefsakr <shereef.sakr@gmail.com>
2017-01-02 18:11:04 +0200

    Weblogic 12c Installation script

---
+++ weblogic12c/install-weblogic12c.bat
@@ -0,0 +1,14 @@
+pushd E:\WebLogic12c\DevDomain
+
+set JAVA_HOME="C:\Program Files\Java\jdk1.7.0_79"
+
+set JAVA_BIN="C:\Program Files\Java\jdk1.7.0_79\bin\java.exe"
+
+set MW_HOME=D:\Development\Weblogic12c-121200DevDomain
+
+set JAVA_VENDOR=Sun
+
+rem java -version
+rem bkp from environment variables: C:\ProgramData\Oracle\Java\javapath;
+
+%JAVA_BIN% -jar "D:\software-sources\Development\ApplicationServer\Weblogic12.1.2\wls_121200.jar"
